87c73cbd0982ed31665d2632a489caac939091fb
Nathan <nhogge@gmail.com>
2017-02-19 12:05:24 -0500

    tape and camera lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.networktables.*;
@@ -13,78 +15,181 @@ public class Main {
     System.loadLibrary("opencv_java310");
     
     String local_ip = "127.0.0.1";
+    InetAddress IP = null;
+	try {
+		IP = InetAddress.getLocalHost();
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	if(IP != null) {
+		local_ip = IP.getHostAddress();
+		System.out.println("IP of my system is := "+IP.getHostAddress());
+	}
 
     // Connect NetworkTables, and get access to the publishing table
     NetworkTable.setClientMode();
     // Set your team number here
-    //NetworkTable.setTeam(9999);
+    //NetworkTable.setTeam(3707);
     NetworkTable.setIPAddress("127.0.0.1");
     NetworkTable.initialize();
+    
+    //windows-x86_64_2015
 
     VideoCapture camera1 = startCamera(0);
-    CvSource imageSource1 = startStream("rear", local_ip, 1186);
+    CvSource imageSource1 = startStream("rear", local_ip, 1188);
     
-//    VideoCapture camera2 = startCamera(1);
-//    CvSource imageSource2 = startStream("front", local_ip, 1187);
+    VideoCapture camera2 = startCamera(1);
+    CvSource imageSource2 = startStream("front", local_ip, 1187);
 
     // All Mats and Lists should be stored outside the loop to avoid allocations
     // as they are expensive to create
     Mat inputImage = new Mat();
-    Mat hsv = new Mat();
-    int i = 0;
     GripPipeline pipeline = new GripPipeline();
+    
+    boolean camerasConnected = false;
     
     // Infinitely process image
     while (true) {
-    	if(camera1.read(inputImage)) {
-    		pipeline.process(inputImage);
-    		Imgproc.drawContours(inputImage, pipeline.filterContoursOutput(), -1, new Scalar(0, 255, 0), 3);
-    		imageSource1.putFrame(inputImage);
-    		
-    		calculateGear(pipeline.filterContoursOutput());
+    	camerasConnected = readCameras(camera1, camera2, imageSource1, imageSource2, inputImage, pipeline);
+    	
+    	if(!camerasConnected) {
+    		System.out.println("NOPE");
     	}
-//    	if(camera2.read(inputImage)) {
-//    		//System.out.println("read" + Integer.toString(i));
-//    		imageSource2.putFrame(inputImage);
-//    		//i++;
-//    	}
     }
     
   }
   
-  private static void calculateGear(ArrayList<MatOfPoint> contours) {
-	  ArrayList<Double> centerX = new ArrayList<Double>();
-	  ArrayList<Double> centerY = new ArrayList<Double>();
+  private static boolean readCameras(VideoCapture camera1, VideoCapture camera2, CvSource imageSource1, CvSource imageSource2, Mat inputImage, GripPipeline pipeline) {
+	boolean cam1 = false;
+	boolean cam2 = false;
+	
+	cam1 = camera1.read(inputImage);
+	if(cam1) {
+		System.out.println("1");
+		pipeline.process(inputImage);
+		Imgproc.drawContours(inputImage, pipeline.filterContoursOutput(), -1, new Scalar(255, 0, 0), 3);
+		
+		calculateGear(inputImage, pipeline.filterContoursOutput());
+		
+		drawBackupCamera(inputImage);
+		
+		imageSource1.putFrame(inputImage);
+	}
+	
+	cam2 = camera2.read(inputImage);
+	if(cam2) {
+		System.out.println("2");
+		pipeline.process(inputImage);
+		Imgproc.drawContours(inputImage, pipeline.filterContoursOutput(), -1, new Scalar(255, 0, 0), 3);
+		
+		calculateGear(inputImage, pipeline.filterContoursOutput());
+		
+		drawBackupCamera(inputImage);
+		
+		imageSource2.putFrame(inputImage);
+	}
+	
+	return cam1 && cam2;
+  }
+  
+  private static void drawBackupCamera(Mat inputImage) {
+	  Imgproc.line(inputImage, new Point(140,40), new Point(180,40), new Scalar(255, 255, 255));
+	  Imgproc.line(inputImage, new Point(145,60), new Point(175,60), new Scalar(255, 255, 255));
+	  Imgproc.line(inputImage, new Point(150,80), new Point(170,80), new Scalar(255, 255, 255));
+	  
+	  Imgproc.line(inputImage, new Point(150,160), new Point(170,160), new Scalar(0, 255, 0));
+	  Imgproc.line(inputImage, new Point(145,180), new Point(175,180), new Scalar(0, 255, 255));
+	  Imgproc.line(inputImage, new Point(140,200), new Point(180,200), new Scalar(0, 0, 255));
+	  
+	  Imgproc.line(inputImage, new Point(20,220), new Point(100,150), new Scalar(255, 255, 255));
+	  Imgproc.line(inputImage, new Point(300,220), new Point(220,150), new Scalar(255, 255, 255));
+  }
+  
+  private static void calculateGear(Mat inputImage, ArrayList<MatOfPoint> contours) {
+	  
+	  Tape bestTape = null;
+	  MatOfPoint bestContour = null;
 	  
 	  for (MatOfPoint c : contours) {
+		  
 		  Rect cRect = Imgproc.boundingRect(c);
 		  
-		  int cX = Math.round((cRect.x + cRect.width)/2);
-		  int cY = Math.round((cRect.y + cRect.height)/2);
-		  double cRatio = cRect.width / cRect.height;
+		  //check if its a tall piece of tape
+		  if(Tape.isValidGearTape(cRect)) { 
+			  //System.out.println("*****GOOD ratio");
+			  
+			  Tape tape1 = new Tape();
+			  tape1.setRect(cRect);
+			  
+			  //see if there is another valid tape to the right
+			  Tape tape2 = new Tape();
+			  for(MatOfPoint c2 : contours) {
+				  if(c != c2) {
+					  tape2.setRect(Imgproc.boundingRect(c2));
+					  if(tape1.checkNeighborGearTape(tape2)) {
+						  break;
+					  }
+				  }
+			  }
+			  
+			  //if we have a neighbor tape, set this tape to best and stop checking others
+			  if(tape1.hasNeighbor) {
+				  bestTape = tape1;
+				  bestContour = c;
+				  break;
+			  }
+			  //else if this is the first validTape, use it but keep looking
+			  else if(bestTape == null) {
+				  bestTape = tape1;
+				  bestContour = c;
+			  }
+		  }
 		  
-		  centerX.add(new Double(cX));
-		  centerY.add(new Double(cY));
 	  }
 	  
-	  writeNetworkTables(centerX);
-  }
-  private static void writeNetworkTables(ArrayList<Double> centerX) {
-	  NetworkTable table = NetworkTable.getTable("DogVision");
-	  ITable calcTable = table.getSubTable("gear");
+	  if(bestTape != null) {
+		  bestTape.calculateDistanceGear();
+		  
+		  //write the contour that we thought was best to image
+		  ArrayList<MatOfPoint> best = new ArrayList<MatOfPoint>();
+		  best.add(bestContour);
+		  Imgproc.drawContours(inputImage, best, -1, new Scalar(0, 255, 0), 3);
+		  
+		  int cnt = 1;
+		  if(bestTape.hasNeighbor) {
+			  cnt = 2;
+		  }
+		  
+		  //write to network tables
+		  writeNetworkTables("gear", bestTape.distance, bestTape.centerX, bestTape.centerY, cnt, true);
+	  }
+	  else {
+		  writeNetworkTables("gear", 0, 0, 0, 0, false);
+	  }
 	  
-	  calcTable.putNumberArray("centerX", toDoubleArr(centerX));
+	  
+  }
+  private static void writeNetworkTables(String target, int distance, int centerX, int centerY, int tapeCount, boolean locked) {
+	  NetworkTable table = NetworkTable.getTable("DogVision");
+	  ITable calcTable = table.getSubTable(target);
+	  
+	  calcTable.putNumber("distance", (double)distance);
+	  calcTable.putNumber("centerX", (double)centerX);
+	  calcTable.putNumber("centerY", (double)centerY);
+	  calcTable.putNumber("tapeCount", (double)tapeCount);
+	  calcTable.putBoolean("locked", locked);
   }
   
   private static VideoCapture startCamera(int device) {
 	VideoCapture camera = new VideoCapture(device);
-	camera.set(Videoio.CAP_PROP_FRAME_WIDTH, 640);
-	camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480);
+	camera.set(Videoio.CAP_PROP_FRAME_WIDTH, 320);
+	camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, 240);
 	
 	return camera;
   }
   private static CvSource startStream(String webcam_name, String local_ip, int local_port) {
-	CvSource imageSource = new CvSource(webcam_name, VideoMode.PixelFormat.kMJPEG, 640, 480, 30);
+	CvSource imageSource = new CvSource(webcam_name, VideoMode.PixelFormat.kMJPEG, 320, 240, 30);
 	MjpegServer cvStream = new MjpegServer(webcam_name, local_port);
 	cvStream.setSource(imageSource);
 	
---
+++ src/main/java/Tape.java
@@ -0,0 +1,57 @@
+import org.opencv.core.Rect;
+
+public class Tape {
+	public boolean hasNeighbor = false;
+	public int distanceNeighbor = 0;
+	
+	public int distance = 0;
+	public int angle = 0;
+	public int centerX = 0;
+	public int centerY = 0;
+	public double ratio = 0;
+	public Rect rect = null;
+	
+	public void setRect(Rect newRect) {
+		this.rect = newRect;
+		this.centerX = Math.round((rect.x + rect.width)/2);
+		this.centerY = Math.round((rect.y + rect.height)/2);
+		this.ratio = rect.width / rect.height;
+	}
+	
+	public boolean checkNeighborGearTape(Tape tape2) {
+		boolean yMatch = false;
+		boolean xMatch = false;
+		
+		if((this.centerY + 10) > tape2.centerY && 
+		   (this.centerY - 10) < tape2.centerY) {
+			yMatch = true;
+		}
+		if(((this.rect.height * 1.6) + this.centerX - 30) < tape2.centerX && 
+		   ((this.rect.height * 1.6) + this.centerX + 30) > tape2.centerX) {
+			xMatch = true;
+		}
+		
+		this.hasNeighbor = yMatch && xMatch;
+//		if(this.hasNeighbor) {
+//			tape2.calculateDistanceGear();
+//			this.distanceNeighbor = tape2.distanceNeighbor;
+//		}
+		
+		return this.hasNeighbor;
+	}
+	
+	public void calculateDistanceGear() {
+		this.distance = (int) Math.round( (5 * 240) / (2 * this.rect.height * Math.tan(50)) );
+	}
+	
+	static public boolean isValidGearTape(Rect newRect) {
+		double testRatio = ((double)newRect.width / (double)newRect.height);
+		//System.out.println(testRatio);
+		if(testRatio > 0.3 && testRatio < 0.7) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
